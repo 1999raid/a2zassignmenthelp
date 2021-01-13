bb36a77b55928af454fe1f551ff91d327ec96fa4
Rajneesh <Rajneeshd860@gmail.com>
2021-01-13 12:31:01 +0530

    hello commit

---
+++ resources/views/user/welcome.blade.php
@@ -0,0 +1,75 @@
+@extends('user.layout.master')
+   @section('content')
+     <div class="container counter_head_div">
+    <div class="col-sm-4 counter_div">
+    <div class="counter_inner_div">
+    <h1 class="counter" style="color:#FF7F50;">50,000+</h1>
+    <p class="counter_content">Assignments</p>
+    </div>
+    </div>
+    <div class="col-sm-4 counter_div">
+    <div class="counter_inner_div">
+    <h1 class="counter" style="color:#FF7F50;">1124+</h1>
+    <p class="counter_content">PHD and Masters level experts</p>
+    </div>
+    </div>
+    <div class="col-sm-4 counter_div">
+    <div class="counter_inner_div">
+    <h1 class="counter" style="color:#FF7F50;">129+</h1>
+    <p class="counter_content">Subjects</p>
+    </div>
+    </div>
+    </div>
+<div class=" container-fluid services">
+<h1 style="text-align:center;color:#696969">Assignment Help Services</h1>
+  <div class="col-sm-4 services_div">
+  <div class="services_inner_div">
+  <h4 style="color:#696969;">Online Assignment Help</h4>
+  </div>
+  </div>
+  <div class="col-sm-4 services_div">
+  <div class="services_inner_div">
+  <h4 style="color:#696969;">Online Assignment Help</h4>
+  </div>
+  </div>
+  <div class="col-sm-4 services_div">
+  <div class="services_inner_div">
+  <h4 style="color:#696969;">Online Assignment Help</h4>
+  </div>
+  </div>
+</div>
+<br><br>
+<div class=" container-fluid key_feature">
+<h1 style="text-align:center;color:#696969;">Key Features</h1>
+  <div class="col-sm-4 services_div">
+  <div class="services_inner_div">
+  <h4 style="color:#696969;">On Time Delivery</h4>
+  </div>
+  </div>
+  <div class="col-sm-4 services_div">
+  <div class="services_inner_div">
+  <h4 style="color:#696969;">24x7 Live Chat Help Support</h4>
+  </div>
+  </div>
+  <div class="col-sm-4 services_div">
+  <div class="services_inner_div">
+  <h4 style="color:#696969;">4205 PHD Experts</h4>
+  </div>
+  </div>
+  <div class="col-sm-4 services_div">
+  <div class="services_inner_div">
+  <h4 style="color:#696969;">Services For All Subjects</h4>
+  </div>
+  </div>
+  <div class="col-sm-4 services_div">
+  <div class="services_inner_div">
+  <h4 style="color:#696969;">Best Price Guarantee</h4>
+  </div>
+  </div>
+  <div class="col-sm-4 services_div">
+  <div class="services_inner_div">
+  <h4 style="color:#696969;">Plagrism Free Work</h4>
+  </div>
+  </div>
+</div>
+  @endsection
